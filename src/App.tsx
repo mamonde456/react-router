f9b0f27f8621be7aa0f03ad0b9a7a415430c2666
mamonde456 <mamonde456@gmail.com>
2023-10-05 16:36:20 +0900

    fix: merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-function App() {
-  return <></>;
-=======
 import Route from "./router/Route";
 import Router from "./router/Router";
 import Home from "./components/Home";
@@ -14,7 +10,6 @@ function App() {
       <Route path="/about" components={<About />}></Route>
     </Router>
   );
->>>>>>> d87a9a8a94446d0bab713d983d3c341a72b11534
 }
 
 export default App;
